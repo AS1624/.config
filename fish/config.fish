8779c8cd39b927b00e4457b52832cf43ac541856
AS1624 <thebest1dudes@gmail.com>
2025-03-28 21:09:11 -0800

    add as stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 alias please="sudo (history --max 1)"
+alias token="cat ~/.token | xclip -selection clipboard"
