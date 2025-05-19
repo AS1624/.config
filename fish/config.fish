968e3c9ef39686c8f34e0c6186ebf2640271bf8b
AS1624 <thebest1dudes@gmail.com>
2025-05-18 18:43:45 -0800

    pink for amelia

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+starship init fish | source
+
 alias please="sudo (history --max 1)"
 alias token="cat ~/.token | xclip -selection clipboard"
 
